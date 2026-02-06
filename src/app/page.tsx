5945e31e025718e7f02ac79769a938c3402b432c
Daniel-Cervera <daniel.cervera.2029@gmail.com>
2026-02-05 18:43:32 -0600

    Cargue el inicio de proyecto - My Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,28 @@
 import Image from "next/image";
+import { redirect } from 'next/navigation';
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
-}
+
+// Con este server component hacemos que nuestros usarios
+// sean redirijidos al igual como podemos ver que nos genera
+// los codigos de estado HTTP 303/307 son principalmente codigos de reedireccion
+
+// 303 : 'See Other' 'Ver Otro'
+// Este estandar HTTP indica que el recurso que vemos
+// se encuentra en otra parte. Esto se usa para prevenir que el contenido
+// sea reenviado cuando el usario recarga la pagina.
+// Caracteristica: El metodo solicitado cambia a GET (independientemente del metodo orginal)
+// Ejemplo : Si un usuario envia un formulario de registro (POST) y quieres enviarlo
+// a una pagina de 'Exito'(GET), usas 303. Esto asegura que si el usario refresca
+// la pagina de exito, no reenvie el formulario.
+
+
+// 307 : 'Temporary redirect' 'Redireccionamiento Temporal'
+// Indica que el recurso solicitado se ha movido temporalmente a una URL diferente
+// , pero mantiene el metodo (POST, PUT, DELETE) y el cuerpo de la peticion se 
+// se conserven. Es el sustituto moderno y estricto de codigo 302.
+// Caracteristica: El cliente (nevegador) no debe cambiar el metodo de la solicitud al redirigir.
+// Ejemplo: Redirecciones temporales de API o paginas de error.
+
+
+redirect('/dashboard/counter'); 
+
